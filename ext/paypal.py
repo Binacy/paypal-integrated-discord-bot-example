f005d3789d3bfc86af93eec88604c408d576719d
Gaurav Rao <70203081+GauravWTF@users.noreply.github.com>
2023-07-13 21:11:40 +0530

    Update paypal.py

--- ext/paypal.py
+++ ext/paypal.py
@@ -1,6 +1,6 @@
 import aiohttp, config, asyncio
 from discord.ext import commands
-from models import Transactions, Products
+from models import Transactions, Products, Role_Products, Role_Transactions
 
 
 class paypal(commands.Cog):
@@ -9,6 +9,37 @@ class paypal(commands.Cog):
         self.bot.temp_token = None
         self.bot.loop.create_task(self.token_refresher())
         self.bot.loop.create_task(self.product_sender())
+        self.bot.loop.create_task(self.role_sender())
+
+    async def role_sender(self):
+        records = await Role_Transactions.filter(paid=False).all()
+        for t in records:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    f"https://api.sandbox.paypal.com/v2/invoicing/invoices/{t.payapl_id}"
+                    ) as resp:
+                    data = await resp.json()
+                    if data["status"] == "PAID":
+                        t.paid = True
+                        await t.save()
+                        try:
+                            product = await Role_Products.get(role_id=t.role_id)
+                        except:
+                            continue
+                        guild = self.bot.get_guild(product.guild_id)
+                        if guild:
+                            role = guild.get_role(product.role_id)
+                            if role:
+                                try:
+                                    member = guild.get_member(t.user_id) or await guild.fetch_member(t.user_id)
+                                except:
+                                    continue
+                                try:
+                                    await member.add_roles(role)
+                                except:
+                                    continue
+        await asyncio.sleep(20)
+        self.bot.loop.create_task(self.role_sender())
 
     async def product_sender(self):
         records = await Transactions.filter(paid=False).all()
